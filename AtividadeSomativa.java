917cadb81b4e4b6185c537a1a04df1c166a164d1
otaviolimaaa <159277831+otaviolimaaa@users.noreply.github.com>
2024-03-13 11:33:59 -0300

    primeiro commit

---
+++ AtividadeSomativa.java
@@ -0,0 +1,12 @@
+package exercicios1303;
+
+public class AtividadeSomativa {
+
+	public static void main(String[] args) {
+    String espacos = "Tedro Tescava Teixes no Tier";
+    String corrigir = espacos.replace("T","P");
+    System.out.println("Antes: " + espacos + "Depois: " + corrigir);
+
+	}
+
+}
---
+++ Atividadesomativa2.java
@@ -0,0 +1,15 @@
+package exercicios1303;
+
+public class Atividadesomativa2 {
+
+	public static void main(String[] args) {
+	String espaco = "Pedro Pescava Peixes no Pier";
+	String antes = espaco.substring (14, 19);
+	
+	System.out.println ("Palavra: " + antes);
+	
+	
+
+	}
+
+}
